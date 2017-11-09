c9447a35b1191aa177bff9b7d7786d47bf8b3061
vladimir-k <vladimir-k@yandex-team.ru>
2017-11-09 09:53:30 +0300

    - adding main mvn pom  - removing some components

--- persistor/src/main/java/ru/spb/luxoft/vertx/webinar/bnb/Application.java
+++ persistor/src/main/java/ru/spb/luxoft/vertx/webinar/bnb/Application.java
@@ -3,7 +3,6 @@ package ru.spb.luxoft.vertx.webinar.bnb;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
-import io.vertx.ext.dropwizard.DropwizardMetricsOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -20,13 +19,8 @@ public class Application {
 
     @PostConstruct
     public void createClusteredVertx() throws UnknownHostException {
-        final VertxOptions options = new VertxOptions()
-                .setMetricsOptions(new DropwizardMetricsOptions()
-                        .setEnabled(true)
-                        .setJmxEnabled(true)
-                        .setJmxDomain("vertx-metrics"))
-                 /* in embedded mode clustered eventbus is listening on 127.0.0.1 but we need real network ip */
-                .setClusterHost(InetAddress.getLocalHost().getHostAddress());
+        // in embedded mode clustered eventbus is listening on 127.0.0.1 but we need real network ip
+        final VertxOptions options = new VertxOptions().setClusterHost(InetAddress.getLocalHost().getHostAddress());
         Vertx.clusteredVertx(options, this::clusteredVertxCreated);
     }
 
@@ -34,7 +28,6 @@ public class Application {
         if (result.succeeded()) {
             final Vertx clusteredVertx = result.result();
             clusteredVertx.deployVerticle(new Persistor());
-            clusteredVertx.deployVerticle(new TelnetShell());
         }
     }
 
